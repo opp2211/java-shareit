6558dc8d12efd798821b0383fef112a7086840ab
Kirill Maltsev <opp.2211@gmail.com>
2023-06-05 00:47:59 +0500

    feat, refactor: создать модели, дто; перестить классы по пакетам

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Data;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+@Data
 public class ItemDto {
+    private Long id;
+    private String name;
+    private String description;
+    private boolean isAvailable;
+    private User owner;
+    private ItemRequest request;
 }
