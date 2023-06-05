9203af9a3fd71b3c04028afb7ee8860bde89d994
Kirill Maltsev <opp.2211@gmail.com>
2023-06-05 23:04:28 +0500

    feat: создать User и Item мапперы (модель -> дто)

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,10 +1,12 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
 @Data
+@AllArgsConstructor
 public class ItemDto {
     private Long id;
     private String name;
